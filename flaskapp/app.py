7963f160d9f5f80153092b404418a4bea389bb3f
Dagmawe Legesse <dlegesse@google.com>
2025-02-23 18:29:39 -0500

    fixed CORS issue

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app, origins="https://storage.mtls.cloud.google.com/webapp-dlegesse-test/frontend/app.html") 
 
 @app.route("/")
 def hello():
